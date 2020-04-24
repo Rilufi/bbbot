1bbb8aeab61436a66eb0ae23b4cbfd7277b30809
Rilufi <48300685+Rilufi@users.noreply.github.com>
2020-04-24 00:43:45 -0300

    Update bbbot.py

--- bbbot.py
+++ bbbot.py
@@ -32,7 +32,7 @@ print("abrindo página de votação")
 
 def votar():
 
-    driver.get("https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-babu-manu-ou-mari-ef6b3f8d-6cd8-4418-9f63-ca6702323cde.ghtml")
+    driver.get("https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-babu-rafa-ou-thelma-0ffab592-8b60-42aa-b29f-ff828805e7ed.ghtml")
     driver.find_element_by_xpath("""/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div/div[1]""").click() #Clica no Participante
     sleep(2)
     driver.find_element_by_xpath("""/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div/div[2]/img""").click() #Clica no Captcha pela 1Âº vez
